8792fc4067e346853606b8d9c5cc877408362d4c
Ajay Dheeraj <and38@duke.edu>
2019-04-01 03:01:38 -0400

    quad sieve with sequence

--- sieve.py
+++ sieve.py
@@ -44,6 +44,51 @@ def get_sol(x,n,p): #gets solutions to x^2-n = 0 mod p
             sol.append(i)
     return sol
 
+#examine numbers x^2-n for B-Smooth values, x runs integers from ceil(sqrt(n))
+#returns tuple of integer, exponent vector if B-smooth in sequence x^2-n, running from x_start to top_bound
+def sieve_quad_poly(n,B): #n should be odd
+    
+    x_start = int(math.ceil(n**0.5)) #bounds of sieve
+    top_bound = 2 * x_start #bounds of sieve
+    
+    p_set = sieve_era(B)
+    expfac=[ [0]*len(p_set) for i in range(x_start, top_bound)] #exponent vector 
+    
+    orig = [i*i-n for i in range(x_start, top_bound)] #preserved list
+    l = [i*i-n for i in range(x_start,top_bound)] #list of all the corresponding x^2-n that will be modified
+        
+    for p in p_set:
+        sols = get_sol(n,p) #solutions x for x^2-n=0 mod p
+
+        if (p == 2): #solutions to x^2-n = 0 mod 2: if n is even, x = 0 mod 2, if n is odd, x = 1 mod 2
+            index = 0
+            for j in range(1, top_bound, 2):
+                if x_start<=j<top_bound:
+                    num = j-x_start
+                    while(l[j-x_start] % 2 == 0):
+                        expfac[num][index]+=1
+                        l[j-x_start] /= 2
+        elif len(sols)==1:
+            return p
+        elif len(sols)==2:
+            index = p_set.index(p)
+            for j in range(sols[0],top_bound,p): 
+                if x_start<=j<top_bound:
+                    num = j-x_start
+                    while (l[j-x_start] % p == 0):
+                        l[j-x_start] /= p
+                        expfac[num][index] += 1
+            for j in range(sols[1],top_bound,p):
+                if x_start<=j<top_bound:
+                    num = j-x_start
+                    while(l[j-x_start] % p == 0):
+                        l[j-x_start] /= p
+                        expfac[num][index] += 1
+                    
+
+    return [(orig[i],expfac[i]) for i,j in enumerate(l) if j == 1]
+                
+
 def exp(n,B): #return exponent vector for n, w/ primes up to B through trial division
     p_set = sieve_era(B)
     ans = []
