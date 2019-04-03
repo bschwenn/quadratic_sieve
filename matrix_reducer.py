Fix issue large number overflow issue and make more efficient
The trial division factorization of the B-smooth numbers was
broken for large numbers so I added tests and fixed it. Furthermore,
I made the factor matrix such that it only added as columns primes
that actually appear in one of the smooth numnbers; it's much
faster now.

matrix_reducer.py
@@ -7,22 +7,13 @@ def mod_2_representation(mat):
     """
     returns mod 2 representation of the matrix mat
     """
-    for row in mat:
-        for i in range(len(row)):
-            #row[i] = 0 if row[i] % 2 == 0 else 1
-            row[i] %= 2
-    return mat
-
+    return mat % 2
 
 def add_mod_2(r1, r2):
     """
     adds two rows of the matrix mod 2
     """
-    # assert (len(r1) == len(r2), "lengths of two rows are not equal!")
-    for i in range(len(r1)):
-        r1[i] += r2[i]
-        r1[i] %= 2
-    return r1
+    return r1 ^ r2
 
 
 def eliminate(mat):
@@ -69,19 +60,20 @@ def get_solution_rows(mat, marks):
         #print("Solutions:", solutions)
     return solutions
 
-
-
-
 def find_dependencies(mat):
     """
     :param mat: 2d np array where each row is the exponent vector of the prime factorization.
     :return: ret: 2d np array where each row is a vector in the mod 2 left nullspace of mat.
     """
-    #print("Original \n", mat)
     ret = mod_2_representation(mat)
+    #print(np.array(reduce_empties(ret)).shape)
+    #ret2 = np.array(reduce_empties(ret))
+    #ret2, marks = eliminate(ret)
+    #return get_solution_rows(ret2,marks)
     #print("Mod 2 rep: \n", ret)
     ret, marks = eliminate(ret)
     #print(ret)
     #print("Marks")
     #print(marks)
+    #print(get_solution_rows(ret,marks))
     return get_solution_rows(ret, marks)